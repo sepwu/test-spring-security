3fe90e3734c94276b201a30bff0ddbc170811350
Wu Bin <sep_wu@163.com>
2018-09-06 01:22:56 +0800

    remove UserDetailsService bean override two configure methods

--- src/main/java/com/srcgo/test/springsecurity/WebSecurityConfig.java
+++ src/main/java/com/srcgo/test/springsecurity/WebSecurityConfig.java
@@ -1,6 +1,8 @@
 package com.srcgo.test.springsecurity;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -17,4 +19,26 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		return manager;
 	}
 
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        
+        auth.inMemoryAuthentication()
+            .withUser("user").password("user").roles("USER").and()
+            .withUser("admin").password("admin").roles("ADMIN");
+        
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+        .authorizeRequests()
+            .anyRequest().authenticated()
+            .and()
+            .csrf().disable()
+        .formLogin().and()
+        .logout().and()
+        .httpBasic();
+    }
+
+	
 }
