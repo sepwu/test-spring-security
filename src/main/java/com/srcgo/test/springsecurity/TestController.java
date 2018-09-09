4006b2698617c03180f9c20ae12a9df8ad040f80
Wu Bin <sep_wu@163.com>
2018-09-09 14:48:55 +0800

    ~

--- src/main/java/com/srcgo/test/springsecurity/TestController.java
+++ src/main/java/com/srcgo/test/springsecurity/TestController.java
@@ -1,13 +1,23 @@
 package com.srcgo.test.springsecurity;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+    
+    private static final Logger logger = LoggerFactory.getLogger(TestController.class);
 
 	@RequestMapping("/")
 	public String test() {
+	    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+	    authentication.getPrincipal();
+	    logger.debug(authentication.toString());
+	    
 		return "test";
 	}
 }
