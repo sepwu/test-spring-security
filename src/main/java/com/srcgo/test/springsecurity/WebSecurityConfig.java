0da895eb38e27c6566e4b661808e35879a77d40c
WuBin <WuBin@WuBin-PC>
2018-09-10 09:31:57 +0800

    ~

--- src/main/java/com/srcgo/test/springsecurity/WebSecurityConfig.java
+++ src/main/java/com/srcgo/test/springsecurity/WebSecurityConfig.java
@@ -1,20 +1,27 @@
 package com.srcgo.test.springsecurity;
 
-import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-	@Bean
-	public UserDetailsService userDetailsService() {
-		InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-		manager.createUser(User.withUsername("user").password("password").roles("USER").build());
-		return manager;
+	@Override
+ 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+ 		auth.inMemoryAuthentication().withUser("user").password("p").roles("USER")
+ 				.and().withUser("admin").password("p").roles("ADMIN", "USER");
+ 	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			.authorizeRequests().anyRequest().authenticated()
+			.and().formLogin()
+			.and().httpBasic();
 	}
+	
+	
 
 }
